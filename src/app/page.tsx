f7738a26e9739890a90619c99bc10b7d4a4810f0
Ashley06Williams <ashley@waterfalldigital.co.za>
2024-10-23 14:54:45 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
 
   return (
     <div className="flex h-screen w-screen bg-gradient-to-r from-red to-blue-clash items-center justify-center">
-      <div className="bg-blue h-[800px] w-full  md:w-1/2 rounded-3xl shadow-2xl p-2">
+      <div className="bg-blue h-[700px] w-full  md:w-1/2 rounded-3xl shadow-2xl p-2">
         <div className="SEARCH-BAR h-24 w-full p-4">
           <form
             action={handleSubmit}
