27fce6b34601754aa18cdd5d6ac6b1bfefc95908
Ashley06Williams <ashley@waterfalldigital.co.za>
2024-10-29 17:07:08 +0200

    fix/padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -123,7 +123,7 @@ export default function Home() {
 
   return (
     <div className="flex h-screen w-screen bg-gradient-to-r from-red to-blue-clash items-center justify-center ">
-      <div className="bg-blue h-screen md:h-[550px] w-full  md:w-3/4 lg:w-1/2 rounded-3xl shadow-2xl p-2 mt-2 pt-8 md:pt-4">
+      <div className="bg-blue md:h-[550px] w-full  md:w-3/4 lg:w-1/2 rounded-3xl shadow-2xl p-2 mt-2 pt-4 ">
         <div className="SEARCH-BAR h-24 w-full p-4">
           <form
             action={handleSubmit}
